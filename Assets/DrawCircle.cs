8ed3b01a9691e32b914d63b98563e3b6e8d597a9
Alper Canberk <alpercanberk@Alpers-MacBook-Pro.local>
2021-07-12 13:35:03 -0700

    initial commit

---
+++ Assets/DrawCircle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public static class Texture2DExtension
+{
+    public static Texture2D DrawCircle(this Texture2D tex, Color color, int x, int y, int radius = 3)
+    {
+        float rSquared = radius * radius;
+
+        for (int u = x - radius; u < x + radius + 1; u++)
+            for (int v = y - radius; v < y + radius + 1; v++)
+                if ((x - u) * (x - u) + (y - v) * (y - v) < rSquared)
+                    tex.SetPixel(u, v, color);
+
+        return tex;
+    }
+}
